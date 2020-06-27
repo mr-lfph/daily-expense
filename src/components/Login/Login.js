fa336b87e0be63f940a2c4f2c55f47e4a468a9c1
unknown <mr.lfph@gmail.com>
2020-06-27 14:34:28 +0800

    loginstylechange

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Login.css';
 
 const Login = () => {
     return (
         <div>
                 <h2>Login </h2>
                 <button>Signin with Google</button> 
+                <p>or</p>
+                <input placeholder="Enter your Email" type="text" className="rounded w-25" /> <br/>
+               <input  placeholder="Enter your Password" type="password" className="" /> <br/><br/>
+               <button className="btn w-25">Login</button>
         </div>
     );
 };
