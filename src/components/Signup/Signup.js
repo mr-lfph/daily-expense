bc541c96936ae299c70a4c369d1eaa20621477dd
mr-lfph <mr-lfph@gmail.com>
2020-06-28 21:20:23 +0800

    homecommit

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -5,21 +5,6 @@ const Signup = () => {
         <div className="container">
             <h2>Sign Up / Registration Here </h2>
             <br />
-            {/* <div className="col-3 mx-auto">
-                 
-                <div className="row">
-                    Name <input placeholder="Your User Name"></input>
-                </div>
-                <div className="row">
-                    Email <input placeholder="Email"></input>
-                </div>
-                <div className="row">
-                    Password
-                    <input placeholder="Password"></input>
-                </div>
-            </div>
-            */}
-
             <div className="col-12">
                 <div className="row justify-content-center ">
                     <div className="col-3 text-right">Name</div>
