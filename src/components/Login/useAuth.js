d49b3a9d71ac9e70c17b48e925c1a388488ddc25
unknown <mr.lfph@gmail.com>
2020-06-28 19:00:55 +0800

    28.jun

--- src/components/Login/useAuth.js
+++ src/components/Login/useAuth.js
@@ -1,2 +1,33 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
+import * as firebase from 'firebase/app'
+import 'firebase/auth';
+import firebaseConfig from "../../firebaseconfig";
+
+firebase.initializeApp(firebaseConfig);
+//const AuthContext = createContext();
+
+const Auth = () => {
+    const [createUserr, setCreateUserr] = useState(null);
+    //  register user with email and password
+    console.log('auth');
+    const createUse = async (email, password, name) => {
+        console.log('auth11');
+        await firebase.auth().createUserWithEmailAndPassword(email, password, name)
+        .then(res=>{
+            setCreateUserr=res;
+        })
+        .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+            setCreateUserr=null;
+        });
+        setCreateUserr=createUse;
+    }
+    
+    return { createUserr };
+    console.log(createUserr);
+}
+export default Auth;
 
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,47 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import auth from '../Login/useAuth';
+ 
+
 
 const Signup = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const onchangeHandler = e => {
+
+        const { name, value } = e.target;
+        if (name === 'name') {
+            setName(value)
+        }
+        if (name === 'email') {
+            setEmail(value)
+        }
+        if (name === 'password') {
+            setPassword(value)
+        }
+    }
+
+    const createUser = async e => {
+        e.preventDefault();
+        console.log('create');
+
+      
+         const user = auth.createUserr(email, password, name);
+       console.log(user);
+    }
     return (
         <div className="container">
             <h2>Sign Up / Registration Here </h2>
             <br />
-            {/* <div className="col-3 mx-auto">
-                 
-                <div className="row">
-                    Name <input placeholder="Your User Name"></input>
-                </div>
-                <div className="row">
-                    Email <input placeholder="Email"></input>
-                </div>
-                <div className="row">
-                    Password
-                    <input placeholder="Password"></input>
-                </div>
-            </div>
-            */}
+            <form onSubmit={createUser}>
+                <div className="col-12">
+                    <div className="row justify-content-center ">
+                        <div className="col-3 text-right">Name</div>
+                        <div className="col-3 text-left">
+                            <input name="name" value={name} placeholder="User Name" required onChange={onchangeHandler}></input>
+                        </div>
+                    </div>
+                    <div className="row justify-content-center">
+                        <div className="col-3 text-right">Email</div>
+                        <div className="col-3 text-left">
+                            <input name="email" value={email} placeholder="Email" required onChange={onchangeHandler}></input>
+                        </div>
+                    </div>
 
-            <div className="col-12">
-                <div className="row justify-content-center ">
-                    <div className="col-3 text-right">Name</div>
-                    <div className="col-3 text-left">
-                        <input placeholder="Your User Name"></input>
+                    <div className="row justify-content-center">
+                        <div className="col-3 text-right">Password</div>
+                        <div className="col-3 text-left">
+                            <input name="password" value={password} type="password" required placeholder="Password" onChange={onchangeHandler}></input>
+                        </div>
                     </div>
-                </div>
-                <div className="row justify-content-center">
-                    <div className="col-3 text-right">Email</div>
-                    <div className="col-3 text-left">
-                        <input placeholder="Email"></input>
-                    </div>
-                </div>
+                    <br />
+                    <div className="row ">
+                        <div className="col-5"></div>
+                        <div className="col-4">
+                            <button type="submit" className="btn w-100" >SignUp</button>
+                            {/* onClick={createUser} */}
+                        </div>
 
-                <div className="row justify-content-center">
-                    <div className="col-3 text-right">Password</div>
-                    <div className="col-3 text-left">
-                                            
-                        <input type="password" placeholder="Password"></input>
                     </div>
+
                 </div>
-            </div>
+            </form>
         </div>
 
     );
